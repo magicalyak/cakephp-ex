176ab81fc6d56842ee8787df3d7fc559e2e6205c
magicalyak <tgamull@redhat.com>
2016-12-02 09:02:19 -0500

    green deploy

--- app/webroot/index.php
+++ app/webroot/index.php
@@ -3,7 +3,7 @@
   <title>Election Results</title>
  </head>
  <body>
- <?php echo '<p>Hillary wins US Election!</p>'; ?> 
- <?php echo "<font color='red'>Missing Patch</font>"; ?>
+ <?php echo '<p>Trump wins US Election!</p>'; ?> 
+ <?php echo "<font color='green'>Patch Applied</font>"; ?>
  </body>
 </html>
